470430fc221204545a7ddf7e2a9c5a376d1e86dc
Ninenox <nisit15@hotmail.com>
2024-08-04 23:46:46 +0700

    update

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,46 +1,25 @@
-import cv2
-import numpy as np
-import av
-# import mediapipe as mp
-from streamlit_webrtc import webrtc_streamer, WebRtcMode, RTCConfiguration
-# mp_drawing = mp.solutions.drawing_utils
-# mp_drawing_styles = mp.solutions.drawing_styles
-# mp_hands = mp.solutions.hands
-# hands = mp_hands.Hands(
-#     model_complexity=0,
-#     min_detection_confidence=0.5,
-#     min_tracking_confidence=0.5
-# )
-def process(image):
-    image.flags.writeable = False
-    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-    # results = hands.process(image)
-# Draw the hand annotations on the image.
-    image.flags.writeable = True
-    image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
-    # if results.multi_hand_landmarks:
-    #   for hand_landmarks in results.multi_hand_landmarks:
-    #     mp_drawing.draw_landmarks(
-    #         image,
-    #         hand_landmarks,
-    #         mp_hands.HAND_CONNECTIONS,
-    #         mp_drawing_styles.get_default_hand_landmarks_style(),
-    #         mp_drawing_styles.get_default_hand_connections_style())
-    return cv2.flip(image, 1)
-RTC_CONFIGURATION = RTCConfiguration(
-    # {"iceServers": [{"urls": ["stun:stun.l.google.com:19302"]}]} 
-    {"iceServers": [{"urls": ["stun:stun.cloudflare.com:3478"]}]}
+import streamlit as st
+from streamlit_webrtc import webrtc_streamer, WebRtcMode, ClientSettings
+
+st.title("WebRTC Webcam Stream with Streamlit")
+
+# Configure WebRTC client settings
+client_settings = ClientSettings(
+    media_stream_constraints={
+        "video": True,
+        "audio": False,
+    }
 )
-class VideoProcessor:
-    def recv(self, frame):
-        img = frame.to_ndarray(format="bgr24")
-        img = process(img)
-        return av.VideoFrame.from_ndarray(img, format="bgr24")
-webrtc_ctx = webrtc_streamer(
-    key="WYH",
-    mode=WebRtcMode.SENDRECV,
-    rtc_configuration=RTC_CONFIGURATION,
-    media_stream_constraints={"video": True, "audio": False},
-    video_processor_factory=VideoProcessor,
-    async_processing=True,
-)
+
+# Stream the webcam
+def main():
+    
+    webrtc_streamer(
+        key="example",
+        mode=WebRtcMode.SENDRECV,
+        client_settings=client_settings,
+    )
+
+
+if __name__ == "__main__":
+    main()
