5a1266072ed73475f9fed69e412adf2ac81654b6
Ninenox <nisit15@hotmail.com>
2024-08-04 22:46:34 +0700

    update

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,70 +1,15 @@
-"""A sample to use WebRTC in sendonly mode to transfer frames
-from the browser to the server and to render frames via `st.image`."""
-
-import logging
-import queue
-
 import streamlit as st
-from streamlit_webrtc import WebRtcMode, webrtc_streamer
+from streamlit_webrtc import webrtc_streamer, WebRtcMode, ClientSettings
 
-import os
+st.title("WebRTC Webcam Stream with Streamlit")
 
-from twilio.base.exceptions import TwilioRestException
-from twilio.rest import Client
-
-logger = logging.getLogger(__name__)
-
-
-def get_ice_servers():
-    """Use Twilio's TURN server because Streamlit Community Cloud has changed
-    its infrastructure and WebRTC connection cannot be established without TURN server now.  # noqa: E501
-    We considered Open Relay Project (https://www.metered.ca/tools/openrelay/) too,
-    but it is not stable and hardly works as some people reported like https://github.com/aiortc/aiortc/issues/832#issuecomment-1482420656  # noqa: E501
-    See https://github.com/whitphx/streamlit-webrtc/issues/1213
-    """
-
-    # Ref: https://www.twilio.com/docs/stun-turn/api
-    try:
-        account_sid = os.environ["TWILIO_ACCOUNT_SID"]
-        auth_token = os.environ["TWILIO_AUTH_TOKEN"]
-    except KeyError:
-        logger.warning(
-            "Twilio credentials are not set. Fallback to a free STUN server from Google."  # noqa: E501
-        )
-        return [{"urls": ["stun:stun.cloudflare.com:3478"]}]
-
-    client = Client(account_sid, auth_token)
-
-    try:
-        token = client.tokens.create()
-    except TwilioRestException as e:
-        st.warning(
-            f"Error occurred while accessing Twilio API. Fallback to a free STUN server from Google. ({e})"  # noqa: E501
-        )
-        return [{"urls": ["stun:stun.cloudflare.com:3478"]}]
-
-    return token.ice_servers
-
-
-webrtc_ctx = webrtc_streamer(
-    key="video-sendonly",
-    mode=WebRtcMode.SENDONLY,
-    rtc_configuration={"iceServers": get_ice_servers()},
-    media_stream_constraints={"video": True},
+webrtc_streamer(
+    key="example",
+    mode=WebRtcMode.SENDRECV,
+    client_settings=ClientSettings(
+        media_stream_constraints={
+            "video": True,
+            "audio": False,
+        }
+    ),
 )
-
-image_place = st.empty()
-
-while True:
-    if webrtc_ctx.video_receiver:
-        try:
-            video_frame = webrtc_ctx.video_receiver.get_frame(timeout=1)
-        except queue.Empty:
-            logger.warning("Queue is empty. Abort.")
-            break
-
-        img_rgb = video_frame.to_ndarray(format="rgb24")
-        image_place.image(img_rgb)
-    else:
-        logger.warning("AudioReciver is not set. Abort.")
-        break
