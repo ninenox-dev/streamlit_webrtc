a99d360bdc6e725b7da71a9b37a126440f86a921
Ninenox <nisit15@hotmail.com>
2024-08-04 20:13:59 +0700

    update

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,17 +1,36 @@
+"""A sample to use WebRTC in sendonly mode to transfer frames
+from the browser to the server and to render frames via `st.image`."""
+
+import logging
+import queue
+
 import streamlit as st
-from streamlit_webrtc import webrtc_streamer
-import av
+from streamlit_webrtc import WebRtcMode, webrtc_streamer
+
+from sample_utils.turn import get_ice_servers
+
+logger = logging.getLogger(__name__)
 
 
-flip = st.checkbox("Flip")
+webrtc_ctx = webrtc_streamer(
+    key="video-sendonly",
+    mode=WebRtcMode.SENDONLY,
+    rtc_configuration={"iceServers": get_ice_servers()},
+    media_stream_constraints={"video": True},
+)
 
+image_place = st.empty()
 
-def video_frame_callback(frame):
-    img = frame.to_ndarray(format="bgr24")
+while True:
+    if webrtc_ctx.video_receiver:
+        try:
+            video_frame = webrtc_ctx.video_receiver.get_frame(timeout=1)
+        except queue.Empty:
+            logger.warning("Queue is empty. Abort.")
+            break
 
-    flipped = img[::-1,:,:] if flip else img
-
-    return av.VideoFrame.from_ndarray(flipped, format="bgr24")
-
-
-webrtc_streamer(key="example", video_frame_callback=video_frame_callback)
+        img_rgb = video_frame.to_ndarray(format="rgb24")
+        image_place.image(img_rgb)
+    else:
+        logger.warning("AudioReciver is not set. Abort.")
+        break
